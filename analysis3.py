71cb8c10c2129c9603418fcb5459d6ec852923be
Nicolás Nocetti <nnocetti@gmail.com>
2023-12-15 22:40:15 -0300

    ...

--- analysis3.py
+++ analysis3.py
@@ -1,8 +1,8 @@
 import os
 import sys
 from time import time
-import itertools
 from statistics import median, mean, stdev
+from math import dist
 
 from scipy import stats
 
@@ -10,17 +10,10 @@ from deap.tools import ParetoFront
 from deap.benchmarks.tools import igd, diversity, convergence, hypervolume
 
 import matplotlib.pyplot as plt
-from scipy.stats import norm
 
 from show import load_run_file
-
-from math import dist
-
-CXMETHOD = ['mate_aligned']
-CXPB = [0.6]
-INDPB = [0.01]
-MU = [200]
-NGEN = [2500]
+from evaluation import Evaluation
+from greedy import greedy, load_inst_file
 
 
 if __name__ == "__main__":
@@ -31,83 +24,103 @@ if __name__ == "__main__":
         exit(1)
     path = sys.argv[1]
 
+
+    sources, targets = load_inst_file(f'{path}/{os.path.basename(os.path.normpath(path))}.json')
+    last_order_time = max(targets, key=lambda t: t[0])[0]
+    targets = [(last_order_time-t[0],t[1],t[2]) for t in targets]
+
+    eval = Evaluation(sources, targets)
+    gsol = greedy(eval)
+    gpoint = eval.evaluate(gsol)
+
     (dirpath, dirnames, filenames) = next(os.walk(f'{path}/runs'))
     if not filenames:
         print('No Files')
         exit()
 
     ref_front = ParetoFront()
-    puntosx = []
-    puntosy = []
-    raw_data = {}
+    pointsX = []
+    pointsY = []
+    raw_data = []
     for file in filenames:
         (instance, timestamp, runtime, cxmethod, cxpb, indpb, mu, ngen, seed, logbook, pop) = load_run_file(f'{dirpath}\{file}')
         ref_front.update(pop)
-        param_id = ';'.join([str(value) for value in [indpb,cxpb,cxmethod,mu,ngen]])
-        raw_data[param_id] = raw_data[param_id] if param_id in raw_data else []
         run_front = ParetoFront()
         run_front.update(pop)
         x,y = (zip(*[ind.fitness.values for ind in pop]))
-        puntosx.append(x)
-        puntosy.append(y)
+        pointsX.append(x)
+        pointsY.append(y)
         data = {}
         data['timestamp'] = timestamp
         data['runfront'] = run_front
         data['runtime'] = runtime
-        raw_data[param_id].append(data)
+        raw_data.append(data)
 
     ref_front_values = [ind.fitness.values for ind in ref_front]
-    obj1_values, obj2_values = zip(*ref_front_values)
-    nadir_point = (max(obj1_values), max(obj2_values))
+    ref_front_pointsX, ref_front_pointsY = zip(*ref_front_values)
+    nadir_point = (max(ref_front_pointsX), max(ref_front_pointsY))
     ref_hv = hypervolume(ref_front, nadir_point)
 
-    plt.title(path)
-    plt.plot(puntosx,puntosy, "b.")
-    plt.plot(obj1_values, obj2_values, "r.")
-    plt.show()
-    params = [';'.join([str(p) for p in cb]) for cb in itertools.product(INDPB, CXPB, CXMETHOD, MU, NGEN)]
-    rhv = {}
-    for param_id in sorted(raw_data.keys()):
-        if param_id in params:
-            runtime = []
-            rhv[param_id] = []
-            div = []
-            invgd = []
-            pnd = []
-            print(param_id, end=';')
-            for data in raw_data[param_id]:
-                #print(f"{data['timestamp']}.p")
-                runtime.append(data['runtime'])
-                invgd.append(igd([ind.fitness.values for ind in data['runfront']], ref_front_values))
-                #conv = convergence(run_front, ref_front_values)
-                div.append(diversity(data['runfront'], ref_front_values[0], ref_front_values[-1]))
-                hv = hypervolume(data['runfront'], nadir_point)
-                rhv[param_id].append(hv/ref_hv)
-                pnd.append(len(data['runfront']))
+    plt.title('Soluciones')
+    plt.plot(pointsX,pointsY, "b.")
+    plt.plot(ref_front_pointsX, ref_front_pointsY, "r.")
+    plt.tight_layout()
+    plt.savefig(f'{path}/solutions.png')
+    plt.cla()
 
-            print(f"{mean(rhv[param_id]):.4f};{stdev(rhv[param_id]):.4f};{mean(runtime):.1f};{stdev(runtime):.2f};{mean(invgd):.4f};{stdev(invgd):.4f};{mean(div):.4f};{stdev(div):.4f};{min(obj1_values):.4f};{min(obj2_values):.4f};{mean(pnd):.4f};{stdev(pnd):.4f};")
-            loc, scale = norm.fit(rhv[param_id])
-            Hiper = norm(loc=loc, scale=scale)
-            loc, scale = norm.fit(invgd)
-            # create a normal distribution with loc and scale
-            Generation = norm(loc=loc, scale=scale)
-            loc, scale = norm.fit(runtime)
-            # create a normal distribution with loc and scale
-            run = norm(loc=loc, scale=scale)
-            loc, scale = norm.fit(div)
-            # create a normal distribution with loc and scale
-            Diver = norm(loc=loc, scale=scale)
-            loc, scale = norm.fit(pnd)
-            # create a normal distribution with loc and scale
-            puntos = norm(loc=loc, scale=scale)
-            print(f"{stats.kstest(rhv[param_id], Hiper.cdf).pvalue:.2E};{stats.kstest(invgd, Generation.cdf).pvalue:.2E};{stats.kstest(runtime,run.cdf).pvalue:.2E};{stats.kstest(div,Diver.cdf).pvalue:.2E};{stats.kstest(pnd, puntos.cdf).pvalue:.2E}")
-            print(f"{stats.normaltest(rhv[param_id]).pvalue};{stats.normaltest(invgd).pvalue};{stats.normaltest(runtime).pvalue};{stats.normaltest(div).pvalue};{stats.normaltest(pnd).pvalue}")
-        distancia = 50000;
-        punto = [0,0]
-        ref = [min(obj1_values),min(obj2_values)]
-        for p in ref_front_values:
-            distActual = dist(p,ref)
-            if distActual<distancia:
-                distancia=distActual
-                punto = p
-        print(f"{punto}")
+    runtime = []
+    invgd = []
+    #gd = []
+    #pnd = []
+    div = []
+    rhv = []
+    comp_points = []
+    for data in raw_data:
+        #print(f"{data['timestamp']}.p")
+        runfront = data['runfront']
+        run_values = [ind.fitness.values for ind in runfront]
+        runtime.append(data['runtime'])
+        invgd.append(igd(run_values, ref_front_values))
+        #gd.append(convergence(run_front, ref_front_values))
+        #pnd.append(len(runfront))
+        div.append(diversity(runfront, ref_front_values[0], ref_front_values[-1]))
+        hv = hypervolume(runfront, nadir_point)
+        rhv.append(hv/ref_hv)
+
+
+        min_distance = float('inf')
+        point = None
+        for p in run_values:
+            distance = dist(p, gpoint)
+            if distance < min_distance:
+                min_distance = distance
+                point = p
+        comp_points.append(point)
+    t1 = [v1 for v1, _ in comp_points]
+    t2 = [v2 for _, v2 in comp_points]
+
+    print(f"{median(rhv):.4f};{mean(rhv):.4f};{stdev(rhv):.4f};{stats.normaltest(rhv).pvalue:.3f}")
+    print(f"{median(invgd):.2f};{mean(invgd):.2f};{stdev(invgd):.4f};{stats.normaltest(invgd).pvalue:.3f}")
+    #print(f"{median(gd):.2f};{mean(gd):.2f};{stdev(gd):.4f};{stats.normaltest(gd).pvalue:.3f}")
+    #print(f"{median(pnd):.2f};{mean(pnd):.2f};{stdev(pnd):.4f};{stats.normaltest(pnd).pvalue:.3f}")
+    print(f"{median(div):.4f};{mean(div):.4f};{stdev(div):.4f};{stats.normaltest(div).pvalue:.3f}")
+    print(f"{median(runtime):.4f};{mean(runtime):.1f};{stdev(runtime):.2f};{stats.normaltest(runtime).pvalue:.3f}")
+    print(f"{median(t1):.1f};{mean(t1):.1f};{stdev(t1):.1f};{stats.normaltest(t1).pvalue:.3f}")
+    print(f"{median(t2):.1f};{mean(t2):.1f};{stdev(t2):.1f};{stats.normaltest(t2).pvalue:.3f}")
+
+    print(f"{gpoint[0]:.1f};{median(t1):.1f};{(gpoint[0]-median(t1))*100/gpoint[0]:.1f}")
+    print(f"{gpoint[1]:.1f};{median(t2):.1f};{(gpoint[1]-median(t2))*100/gpoint[1]:.1f}")
+
+    fig = plt.figure()
+    fig.subplots_adjust(wspace=0.60)
+    for idx, (title,metric) in enumerate([('RHV',rhv), ('IGD',invgd), ('SPREAD',div), ('RT',runtime)]):
+        ax = fig.add_subplot(1,4, idx+1)
+        ax.set_title(title)
+        ax.set_xticks([])
+        mmean = mean(metric)
+        mstddev = stdev(metric)
+        ax.vlines([1], mmean-mstddev, mmean+mstddev, color=[0.12156863,0.46666667,0.70588235,1], linestyle='-', lw=6)
+        showmeans = ax.violinplot(metric, showmedians=True, showmeans=True)
+        showmeans['cmedians'].set_color('C1')
+    plt.tight_layout()
+    plt.savefig(f'{path}/violin.png')
